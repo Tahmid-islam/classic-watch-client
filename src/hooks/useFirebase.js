78880def447fec64c0ffe826028d299374cf518f
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-11-11 11:48:07 +0600

    use firebase setup done

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,144 @@
+import { useState, useEffect } from "react";
+import initializeFirebase from "../Pages/Login/Firebase/firebase.init";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+// initialize firebase app
+initializeFirebase();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  // Sign in with Google
+  const signInWithGoogle = (location, history) => {
+    setIsLoading(true);
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        saveUser(user.email, user.displayName, "PUT");
+        setError("");
+        const destination = location?.state?.from || "/";
+        history.replace(destination);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // Register User
+  const registerUser = (email, password, name, location, history) => {
+    setIsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        setError("");
+        const newUser = { email, displayName: name };
+        setUser(newUser);
+        //save user to database
+        saveUser(email, name, "POST");
+        // update user name
+        setUserName(name);
+        const destination = location?.state?.from || "/";
+        history.replace(destination);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // Update user profile
+  const setUserName = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then((result) => {
+        console.log(result);
+        setError("");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  // Login user
+  const loginUser = (email, password, location, history) => {
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setError("");
+        const destination = location?.state?.from || "/";
+        history.replace(destination);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  //  Observe user state that user login or not
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
+    });
+    return unsubscribe;
+  }, [auth]);
+
+  // Sign out user
+  const logout = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser({});
+        setError("");
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // Save user information to database
+  const saveUser = (email, displayName, method) => {
+    const user = { email, displayName };
+    fetch("http://localhost:5000/users", {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  return {
+    user,
+    isLoading,
+    registerUser,
+    loginUser,
+    signInWithGoogle,
+    logout,
+    error,
+  };
+};
+
+export default useFirebase;
---
+++ src/pages/Login/Firebase/firebase.init.js
@@ -0,0 +1,5 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+const initializeFirebase = () => initializeApp(firebaseConfig);
+export default initializeFirebase;
